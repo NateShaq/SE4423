a40472622eb02e70f001ad3b7c71fc3dccfbeb8b
Nathaniel Brock <NateBrock@gamingcapitalgroup.com>
2023-11-19 13:50:57 -0600

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+		System.out.println("The conflict was resolved!");
+	    System.out.println("A bug is fixed on bug_fixing branch!");
+	}
+
+}
